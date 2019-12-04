d76ef9e82a57a67f3e090c60141615c07510f6a8
Selçuk Kütük <selcukkutuk@outlook.com>
2019-12-05 00:33:43 +0300

    Init

---
+++ BlazorHackerNews/Models/ItemModel.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using System.Collections.Generic;
+
+namespace BlazorHackerNews.Models
+{
+
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int? Points { get; set; }
+        public string? User { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("time_ago")]
+        public string? TimeAgo { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("comments_count")]
+        public int? CommentsCount { get; set; }
+        public string? Content { get; set; }
+        public List<ItemModel>? Comments { get; set; }
+        public bool? Deleted { get; set; }
+        public string? Type { get; set; }
+        public string? Url { get; set; }
+        public string? Domain { get; set; }
+    }
+}
---
+++ BlazorHackerNews/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+namespace BlazorHackerNews.Models
+{
+    public class UserModel
+    {
+        public string? Id { get; set; }
+        public string? About { get; set; }
+        public int Karma { get; set; }
+        public string? Created { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("created_time")]
+        public int CreatedTime { get; set; }
+    }
+}
