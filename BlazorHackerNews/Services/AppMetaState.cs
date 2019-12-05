0eec55e9681910ec157c347643e1ecd38ecf3b0d
Selçuk Kütük <selcukkutuk@outlook.com>
2019-12-05 12:30:49 +0300

    added page title capability

---
+++ BlazorHackerNews/Services/AppMetaState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BlazorHackerNews.Services
+{
+    public class AppMetaState
+    {
+        private string _appTitle = "Blazor HackerNews";
+
+        public string Title { get; private set; }
+
+        public void SetTitle(string title)
+        {
+            Title = title + " - " + _appTitle;
+        }
+
+        public Action CallStateHasChanged;
+    }
+}
--- BlazorHackerNews/Startup.cs
+++ BlazorHackerNews/Startup.cs
@@ -1,3 +1,5 @@
+using BlazorHackerNews.Services;
+using BlazorHackerNews.Shared;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,10 +9,12 @@ namespace BlazorHackerNews
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<AppMetaState>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
         {
+            app.AddComponent<AppMeta>("head");
             app.AddComponent<App>("app");
         }
     }
